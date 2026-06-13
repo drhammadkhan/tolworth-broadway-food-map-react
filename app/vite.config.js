bb5a9115f85262aa052fbf0536fb495b5011b0cd
drhammadkhan <98495616+drhammadkhan@users.noreply.github.com>
2026-06-13 18:13:25 +0100

    Update vite.config.js

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/tolworth-broadway-food-map-react/',
   plugins: [react()],
   server: {
     // This project lives in a Dropbox/CloudStorage folder where native FS
